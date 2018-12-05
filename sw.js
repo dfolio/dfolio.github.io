69bf7b7611527839b606532649298e9220f58abd
david.folio@port-folio <david.folio@gmail.com>
2018-12-05 23:06:57 +0100

    add service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
 ---
 permalink: sw.js
 ---
-var CACHE_NAME="dfolio-cache-v1",urlsToCache=["/","/assets/css/main.css"];console.log("loading sw"),self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(e){return console.log("Opened cache"),e.addAll(urlsToCache)}))}),self.addEventListener("fetch",function(s){s.respondWith(caches.match(s.request).then(function(e){return e||fetch(s.request)}))});
+var CACHE_NAME="dfolio-cache-v1",urlsToCache=["/",
+{%- for page in site.html_pages -%}'{{ page.url }}',{%- endfor -%}
+{%- for col in site.collections -%}
+{%- for cp in col.docs -%}'{{ cp.url }}',{%- endfor -%}
+{%- endfor -%}
+"/assets/css/main.css", "/index.html"];console.log("loading sw"),self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(function(e){return console.log("Opened cache"),e.addAll(urlsToCache)}))}),self.addEventListener("fetch",function(s){s.respondWith(caches.match(s.request).then(function(e){return e||fetch(s.request)}))});
+
+
